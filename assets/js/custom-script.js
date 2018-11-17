cb28ac5ef08ae12dfa70628c85eaea4b1c1ed53d
An Huynh <huynhhongan2908@gmail.com>
2018-11-17 16:26:51 +0700

    Add login function

--- assets/js/custom-script.js
+++ assets/js/custom-script.js
@@ -12,5 +12,30 @@ $(function() {
         var template = Handlebars.compile(source);
         var html = template(data);
         $('#products').html(html);
-    })
+    });
+
+});
+
+$(document).ready(function () {
+    $('#btn-register').on('click', function (e) {
+        e.preventDefault();
+        var fullName = $('#form-register #full_name').val();
+        var password = $('#form-register #password').val();
+        var phoneNumber = $('#form-register #phone_number').val();
+        $.ajax({
+            url: 'http://localhost:3000/api/users',
+            type: 'POST',
+            dataType: 'json',
+            contentType: 'application/json',
+            data : JSON.stringify({
+                'full_name': fullName,
+                'password': password,
+                'phone_number': phoneNumber,
+                'role': 1 //Normal user
+            }),
+            timeout: 10000
+        }).done(function(data) {
+            location.href = "/index.php?page=login";
+        });
+    });
 });
